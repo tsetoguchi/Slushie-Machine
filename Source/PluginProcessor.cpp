063f851e6236e3a8913847204f932a7b0d354a4b
NelwinA <72899960+NelwinA@users.noreply.github.com>
2023-03-16 02:04:57 -0400

    yes

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -193,12 +193,20 @@ void HiLowCutPluginAudioProcessor::getStateInformation (juce::MemoryBlock& destD
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
+
+    juce::MemoryOutputStream mos(destData, true); 
+    apvts.state.writeToStream(mos);
 }
 
 void HiLowCutPluginAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
+    auto tree = juce::ValueTree::readFromData(data, sizeInBytes);
+    if (tree.isValid()) {
+        apvts.replaceState(tree);
+        updateFilters();
+    }
 }
 
 
